880c00b067868caa244a3896c437e94d79d9ef59
Frederic Tanon <ftanon@made-f0Dr7s10.clusters.42paris.fr>
2024-11-19 18:49:41 +0100

    reformat

--- src/main.cpp
+++ src/main.cpp
@@ -21,28 +21,20 @@ int main(void){
 	struct addrinfo hints;
 	struct addrinfo *res;
 
+	// Server Socket
 	memset(&hints, 0, sizeof hints);
-
 	hints.ai_family = AF_UNSPEC;        // IPv4 ou IPv6, indifférent
     hints.ai_socktype = SOCK_STREAM;    // Connexion TCP
     hints.ai_flags = AI_PASSIVE;		// Remplit l'IP automatiquement
-
 	check_return_zero("getaddrinfo", getaddrinfo(NULL, "6667", &hints, &res));
-
-	// 1. Create the socket
 	int socket_fd;
 	socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	check_return_fd("server socket", socket_fd);
-
-	// 2. Bind the socket to hte port
 	check_return_zero("bind", bind(socket_fd, res->ai_addr, res->ai_addrlen));
-
-	// 3. Listen
 	check_return_zero("listen", listen(socket_fd, 5));
-
 	std::cout << std::endl;
 
-	// 4. Accept
+	// Client socket
 	struct sockaddr_storage client_addr;
     socklen_t addr_size;
 	int	client_fd;
@@ -50,7 +42,7 @@ int main(void){
 	client_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &addr_size);
 	check_return_fd("client socket", client_fd);
 
-	// 5. Receive Data
+	// Receive Data
 	int bytes_read;
 	char buffer[1024] = {0};
 

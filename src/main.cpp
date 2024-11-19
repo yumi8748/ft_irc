538d3530b0d0fa81913f55d1ef905f204a29df70
Frederic Tanon <ftanon@made-f0Dr7s10.clusters.42paris.fr>
2024-11-19 19:02:28 +0100

    incorporate poll to unblock events

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include "../includes/Irc.hpp"
+#include <poll.h>
+#include <signal.h>
+
+int signalisation = 0;
 
 void	check_return_zero(std::string string, int return_value)
 {
@@ -16,7 +20,18 @@ void	check_return_fd(std::string string, int return_value)
 		std::cout << "Success : " << string << " fd is : " << return_value << std::endl;
 }
 
+void	sigint_not_blocking(int signal)
+{
+	if (signal == SIGINT)
+	{
+		std::cout << "OKK"<< std::endl;
+		signalisation = 1;
+	}
+}
+
 int main(void){
+	
+	struct pollfd pfds[2];
 	
 	struct addrinfo hints;
 	struct addrinfo *res;
@@ -32,7 +47,8 @@ int main(void){
 	check_return_fd("server socket", socket_fd);
 	check_return_zero("bind", bind(socket_fd, res->ai_addr, res->ai_addrlen));
 	check_return_zero("listen", listen(socket_fd, 5));
-	std::cout << std::endl;
+	pfds[0].fd = socket_fd;
+	pfds[0].events = POLLIN;
 
 	// Client socket
 	struct sockaddr_storage client_addr;
@@ -41,33 +57,52 @@ int main(void){
 	addr_size = sizeof client_addr;
 	client_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &addr_size);
 	check_return_fd("client socket", client_fd);
+	pfds[1].fd = client_fd;
+	pfds[1].events = POLLIN;
 
-	// Receive Data
-	int bytes_read;
-	char buffer[1024] = {0};
-
-    while (1) {
-        bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
-        if (bytes_read == 0) {
-			std::cout << "Client socket " << client_fd << " closed connection." << std::endl;
-            break ;
-        }
-        else {
-    		std::cout << "Client Message: " << buffer;
-
-			const char *msg = "Received.\n";
-            int msg_len = strlen(msg);
-            int bytes_sent;
-            bytes_sent = send(client_fd, msg, msg_len, 0);
-			std::cout << "Server message to client socket " << client_fd << " : " << msg;
-        }
-    }
-
+	while (signalisation == 0)
+	{
+		std::cout << std::endl;
+		std::cout << "before" << std::endl;
+		poll(pfds, 2, 10000);
+		std::cout << "after" << std::endl;
+		std::cout << std::endl;
+		if (pfds[0].revents & POLLIN)
+		{
+			std::cout << "Connect\n";
+			struct sockaddr_storage client_addr;
+			socklen_t addr_size;
+			int	client_fd;
+			addr_size = sizeof client_addr;
+			client_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &addr_size);
+		}		
+		if (pfds[1].revents & POLLIN)
+		{
+			std::cout << "Message\n";
+			int bytes_read;
+			char buffer[1024] = {0};
+			bytes_read = recv(pfds[1].fd, buffer, sizeof(buffer), 0);
+			if (bytes_read == 0) {
+				std::cout << "Client socket " << client_fd << " closed connection." << std::endl;
+				break ;
+			}
+			else {
+				std::cout << "Client Message: " << buffer;
+				const char *msg = "Received.\n";
+				int msg_len = strlen(msg);
+				int bytes_sent;
+				bytes_sent = send(client_fd, msg, msg_len, 0);
+				std::cout << "Server message to client socket " << client_fd << " : " << msg;
+			}
+		}
+	
+	}
 	std::cout << std::endl;
 
 	// 6. CLose server and client socket fd
-    close(client_fd);
-    close(socket_fd);
+     if (pfds[1].fd)
+	    close(pfds[1].fd);
+    close(pfds[0].fd);
 	std::cout << "Closed client and server socket" << std::endl;
 
 	return 0;

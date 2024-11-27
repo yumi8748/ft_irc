f2eaa3b9256f7a2e56a6edb66324504d1a331b7d
Frederic Tanon <ftanon@made-f0Dr7s10.clusters.42paris.fr>
2024-11-27 18:34:43 +0100

    reformatting

--- src/fredcommandParsing.cpp
+++ src/fredcommandParsing.cpp
@@ -9,26 +9,18 @@ void printvector (std::string str)
 
 void	Server::commandParsing(int i, std::string string)
 {
-	// std::cout << "[" << string << "]" << std::endl;
 	std::string buffer = string;
 	int length = buffer.size();
 	buffer.resize(length - 1);
-	// buffer[length - 1] = '\0';
-	// std::cout  << "["  << buffer << "]" << std::endl;
-	// std::cout << length << std::endl;
-	// std::cout << "[" << buffer.size() << "]" << std::endl;
-
 	std::stringstream ss(buffer);
 	std::string tmp;
 	std::vector<std::string> string_array;
 
-	// std::cout << string_array.size() << std::endl;
 	while (std::getline(ss, tmp, ' '))
 	{
 		string_array.push_back(tmp);
 	}
-	// std::cout << string_array.size() << std::endl;
-	// for_each (string_array.begin(), string_array.end(), printvector);
+
 	std::string cmd_array[] = {
 		"/NICK",
 		"/PASS",
@@ -42,27 +34,15 @@ void	Server::commandParsing(int i, std::string string)
 		"/TOPIC",
 		"/MODE"
 	};
-	// if (cmd_array[5] == string_array[0])
-	// {
-	// 	std::cout << "same" << std::endl;
-	// }
-	// else
-	// {
-	// 	std::cout << "[" << string_array[0].size() << "]" << std::endl;
-	// 	std::cout << "[" << cmd_array[5].size() << "]" << std::endl;
-	// 	std::cout << "different" << std::endl;
-	// }
 
 	int len = sizeof(cmd_array) / sizeof(cmd_array[0]);
 	int j = 0;
-	// std::cout << "[" << string_array[0] << "]" << std::endl;
 	while (j < len)
 	{
 		if (cmd_array[j] == string_array[0])
 			break;
 		j++;
 	}
-	// std::cout << j << std::endl;
 	switch(j)
 	{
 		case 0: this->cmdNick(i, string_array); break;
@@ -84,12 +64,10 @@ int		Server::isRegistered(int i)
 {
 	if (this->_clients[i - 1].getNickname().empty() || this->_clients[i - 1].getUsername().empty())
 	{
-		// std::cout << "reg"   << std::endl;
 		return (0);
 	}
 	else
 	{
-		// std::cout << "not reg"   << std::endl;
 		return (1);
 	}
 }
@@ -99,8 +77,7 @@ void	Server::cmdNick(int i, std::vector<std::string> string_array)
 	if (string_array.size() != 2)
 		throw ErrThrow("Argument error");
 	this->_clients[i - 1].setNickname(string_array[1]);
-	// std::cout << this->_clients[i - 1].getNickname() << std::endl;
-	// std::cout << this->_clients[i - 1].getFd() << std::endl;
+	std::cout << "Nickname saved" << std::endl;
 }
 
 void	Server::cmdUser(int i, std::vector<std::string> string_array)
@@ -108,27 +85,27 @@ void	Server::cmdUser(int i, std::vector<std::string> string_array)
 	if (string_array.size() != 2)
 		throw ErrThrow("Argument error");
 	this->_clients[i - 1].setUsername(string_array[1]);
-	std::cout << this->_clients[i - 1].getUsername() << std::endl;
+	std::cout << "Username saved" << std::endl;
 }
 
 void	Server::cmdPass(int i, std::vector<std::string> string_array)
 {
-	std::cout << "checkPassword" << " "  << i << string_array[0] << std::endl;
+	std::cout << "cmdPass" << " : "  << i << string_array[0] << std::endl;
 }
 
 void	Server::cmdQuit(int i, std::vector<std::string> string_array)
 {
-	std::cout << "quitServer" << " "  << i << string_array[0] << std::endl;
+	std::cout << "cmdQuit" << " : "  << i << string_array[0] << std::endl;
 }
 
 void	Server::cmdPrivmsg(int i, std::vector<std::string> string_array)
 {
 	if (this->isRegistered(i) == 0)
 	{
-		std::cout << "not registered"  << std::endl;
+		std::cout << "Client not registered"  << std::endl;
 		return ;
 	}
-	std::cout << "cmdPrivmsg" << " "  << i << string_array[0] << std::endl;
+	std::cout << "cmdPrivmsg" << " : " << string_array[0] << std::endl;
 }
 
 
@@ -139,60 +116,60 @@ void	Server::cmdJoin(int i, std::vector<std::string> string_array)
 {
 	if (this->isRegistered(i) == 0)
 	{
-		std::cout << "not registered"  << std::endl;
+		std::cout << "Client not registered"  << std::endl;
 		return ;
 	}
-	std::cout << "joinChannel" << " "  << i << string_array[0] << std::endl;
+	std::cout << "cmdJoin" << " : " << string_array[0] << std::endl;
 }
 
 void	Server::cmdPart(int i, std::vector<std::string> string_array)
 {
 	if (this->isRegistered(i) == 0)
 	{
-		std::cout << "not registered"  << std::endl;
+		std::cout << "Client not registered"  << std::endl;
 		return ;
 	}
-	std::cout << "partChannel" << " "  << i << string_array[0] << std::endl;
+	std::cout << "cmdPart" << " : " << string_array[0] << std::endl;
 }
 
 void	Server::cmdKick(int i, std::vector<std::string> string_array)
 {
 	if (this->isRegistered(i) == 0)
 	{
-		std::cout << "not registered"  << std::endl;
+		std::cout << "Client not registered"  << std::endl;
 		return ;
 	}
-	std::cout << "kickChannel" << " "  << i << string_array[0] << std::endl;
+	std::cout << "cmdKick" << " : " << string_array[0] << std::endl;
 }
 
 void	Server::cmdInvite(int i, std::vector<std::string> string_array)
 {
 	if (this->isRegistered(i) == 0)
 	{
-		std::cout << "not registered"  << std::endl;
+		std::cout << "Client not registered"  << std::endl;
 		return ;
 	}
-	std::cout << "inviteChannel" << " "  << i << string_array[0] << std::endl;
+	std::cout << "cmdInvite" << " : " << string_array[0] << std::endl;
 }
 
 void	Server::cmdTopic(int i, std::vector<std::string> string_array)
 {
 	if (this->isRegistered(i) == 0)
 	{
-		std::cout << "not registered"  << std::endl;
+		std::cout << "Client not registered"  << std::endl;
 		return ;
 	}
-	std::cout << "topicChannel" << " "  << i << string_array[0] << std::endl;
+	std::cout << "cmdTopic" << " : " << string_array[0] << std::endl;
 }
 
 void	Server::cmdMode(int i, std::vector<std::string> string_array)
 {
 	if (this->isRegistered(i) == 0)
 	{
-		std::cout << "not registered"  << std::endl;
+		std::cout << "Client not registered"  << std::endl;
 		return ;
 	}
-	std::cout << "modeChannel" << " " << i << string_array[0] << std::endl;
+	std::cout << "cmdMode" << " : " << string_array[0] << std::endl;
 }
 
 

92d370f6ea21a7b32dd3316691d0f79687f24f38
Frederic Tanon <ftanon@made-f0Dr7s10.clusters.42paris.fr>
2024-11-27 15:23:05 +0100

    comment handle client command function

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
 #include "../includes/Irc.hpp"
 
-void handleClientCommand(Client* client, const std::string& cmd, const std::vector<std::string>& args, 
-                         const std::vector<Channel*>& channels, const std::vector<Client*>& clients)
-{
-    if (cmd == "KICK" && args.size() >= 2) {
-        handleKickCmd(client, args[0], args[1], channels, clients);
-    } else if (cmd == "INVITE" && args.size() >= 2) {
-        handleInviteCmd(client, args[0], args[1], channels, clients);
-    } else if (cmd == "TOPIC" && args.size() >= 2) {
-        handleTopicCmd(client, args[0], args[1], channels);
-    } else if (cmd == "MODE" && args.size() >= 3) {
-        handleModeCmd(client, args[0], args[1], args[2], channels);
-    } else {
-        client->sendMessage("Error: Invalid command or arguments.");
-    }
-}
+// void handleClientCommand(Client* client, const std::string& cmd, const std::vector<std::string>& args, 
+//                          const std::vector<Channel*>& channels, const std::vector<Client*>& clients)
+// {
+//     if (cmd == "KICK" && args.size() >= 2) {
+//         handleKickCmd(client, args[0], args[1], channels, clients);
+//     } else if (cmd == "INVITE" && args.size() >= 2) {
+//         handleInviteCmd(client, args[0], args[1], channels, clients);
+//     } else if (cmd == "TOPIC" && args.size() >= 2) {
+//         handleTopicCmd(client, args[0], args[1], channels);
+//     } else if (cmd == "MODE" && args.size() >= 3) {
+//         handleModeCmd(client, args[0], args[1], args[2], channels);
+//     } else {
+//         client->sendMessage("Error: Invalid command or arguments.");
+//     }
+// }
 
 int close_and_ret(std::string msg, int srv, int cli){
 	if (srv > 0)

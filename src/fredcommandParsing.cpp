870607a3ebc2b026b82956c9fea78b947300d1c7
Frederic Tanon <ftanon@made-f0Dr7s10.clusters.42paris.fr>
2024-11-30 17:12:58 +0100

    NICK cmd error handling done

--- src/fredcommandParsing.cpp
+++ src/fredcommandParsing.cpp
@@ -76,10 +76,39 @@ int		Server::isRegistered(int i)
 
 void	Server::cmdNick(int i, std::vector<std::string> string_array)
 {
+	std::ostringstream str1;
+	str1 << this->_clients[i - 1].getFd();
+	std::string fd_string = str1.str();
+	std::string msg;
 	if (string_array.size() != 2)
-		throw ErrThrow("Argument error");
+	{
+		msg = "[Error 431] client " + fd_string + " : Wrong number of parameters\n";
+		send(this->_clients[i - 1].getFd(), msg.c_str(), msg.length(), 0);
+		return;
+	}
+	for (std::vector<Client>::iterator it = this->_clients.begin(); it != _clients.end(); it++)
+	{
+		if ((*it).getNickname() == string_array[1])
+		{
+			msg = "[Error 433] client " + fd_string + " " + string_array[1] + " : Nickname is already in use\n";
+			send(this->_clients[i - 1].getFd(), msg.c_str(), msg.length(), 0);
+			return;
+		}
+	}
+	int j = 0;
+	while (string_array[1][j])
+	{
+		if (isalnum(string_array[1][j]) == 0)
+		{
+			msg = "[Error 432] client " + fd_string + " " + string_array[1] + " : Erroneus nickname\n";
+			send(this->_clients[i - 1].getFd(), msg.c_str(), msg.length(), 0);
+			return;
+		}
+		j++;
+	}
 	this->_clients[i - 1].setNickname(string_array[1]);
-	std::cout << "Nickname saved" << std::endl;
+	msg = "Success : Nickname is saved\n";
+	send(this->_clients[i - 1].getFd(), msg.c_str(), msg.length(), 0);
 }
 
 void	Server::cmdUser(int i, std::vector<std::string> string_array)
@@ -108,7 +137,7 @@ void	Server::cmdPass(int i, std::vector<std::string> string_array)
 	std::string msg;
 	if (string_array.size() != 2)
 	{
-		msg = "[Error 461] client " + fd_string + " cmd PASS : Wrong number of parameters\n";
+		msg = "[Error 461] client " + fd_string + " : Wrong number of parameters\n";
 		send(this->_clients[i - 1].getFd(), msg.c_str(), msg.length(), 0);
 		return;
 	}
